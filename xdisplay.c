f13a001d1b468e5b0c2ee62d380150e305f98100
twitham <twitham>
1996-01-23 07:46:32 +0000

    Initial revision

---
+++ xdisplay.c
@@ -0,0 +1,14 @@
+/*
+ * @(#)$Id: xdisplay.c,v 1.1 1996/01/23 07:46:32 twitham Exp $
+ *
+ * Copyright (C) 1994 Jeff Tranter (Jeff_Tranter@Mitel.COM)
+ * Copyright (C) 1996 Tim Witham <twitham@pcocd2.intel.com>
+ *
+ * (see scope.c and the file COPYING for more details)
+ *
+ * This file just causes display.c to produce xdisplay.o instead
+ *
+ */
+
+#define XSCOPE
+#include "display.c"
