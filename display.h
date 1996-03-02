8ac89191b9cba1ae49e4ced99ea75c07598f53e3
twitham <twitham>
1996-03-02 06:59:47 +0000

    Many X11 specifics moved to x11.c.

--- display.h
+++ display.h
@@ -1,16 +1,15 @@
 /*
- * @(#)$Id: display.h,v 1.5 1996/02/22 06:25:53 twitham Exp $
+ * @(#)$Id: display.h,v 1.6 1996/03/02 06:59:47 twitham Exp $
  *
  * Copyright (C) 1996 Tim Witham <twitham@pcocd2.intel.com>
  *
- * (see oscope.c and the file COPYING for more details)
+ * (see the files README and COPYING for more details)
  *
  * display variables and routines available outside of display.c
  *
  */
 
 extern char fontname[];
-extern int color[];
 
 void
 draw_text();
@@ -24,7 +23,7 @@ cleanup();
 void
 mainloop();
 
-int
+void
 opendisplay();
 
 void
