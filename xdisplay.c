b2e29911b3c98c234f2c5b9c5ce41595fb81ba46
twitham <twitham>
1996-03-02 07:10:19 +0000

    Fixed comments.

--- xdisplay.c
+++ xdisplay.c
@@ -1,10 +1,9 @@
 /*
- * @(#)$Id: xdisplay.c,v 1.2 1996/01/31 07:40:37 twitham Exp $
+ * @(#)$Id: xdisplay.c,v 1.3 1996/03/02 07:10:19 twitham Rel1_1 $
  *
- * Copyright (C) 1994 Jeff Tranter (Jeff_Tranter@Mitel.COM)
  * Copyright (C) 1996 Tim Witham <twitham@pcocd2.intel.com>
  *
- * (see oscope.c and the file COPYING for more details)
+ * (see the files README and COPYING for more details)
  *
  * This file just causes display.c to produce xdisplay.o instead
  *
