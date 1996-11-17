5bc8f397ffe026bc4302885eb3e06c1fcfbb7f5d
twitham <twitham>
1996-11-17 22:34:21 +0000

    Moved sound-card specifics to sc_linux.c to make porting easier.

--- oscope.c
+++ oscope.c
@@ -1,5 +1,5 @@
 /*
- * @(#)$Id: oscope.c,v 1.54 1996/10/06 05:43:29 twitham Rel1_2 $
+ * @(#)$Id: oscope.c,v 1.55 1996/11/17 22:34:21 twitham Exp $
  *
  * Copyright (C) 1996 Tim Witham <twitham@pcocd2.intel.com>
  *
@@ -10,12 +10,8 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <sys/soundcard.h>
 #include "oscope.h"		/* program defaults */
 #include "display.h"		/* display routines */
 #include "func.h"		/* signal math functions */
@@ -29,7 +25,6 @@ Signal ch[CHANNELS];
 char *progname;			/* the program's name, autoset via argv[0] */
 char error[256];		/* buffer for "one-line" error messages */
 int quit_key_pressed = 0;	/* set by handle_key() */
-int snd;			/* file descriptor for sound device */
 char buffer[MAXWID * 2];	/* buffer for stereo sound data */
 char junk[SAMPLESKIP];		/* junk data buffer */
 int v_points;			/* pixels in vertical axis */
@@ -39,6 +34,10 @@ int offset;			/* vertical pixel offset to zero line */
 int actual;			/* actual sampling rate */
 int clip = 0;			/* whether we're maxed out or not */
 char *filename;			/* default file name */
+
+extern void init_sound_card();
+extern void set_sound_card();
+extern void close_sound_card();
 
 /* display command usage on standard error and exit */
 void
@@ -82,7 +81,7 @@ file             %s file to load to restore settings and memory
 void
 parse_args(int argc, char **argv)
 {
-  const char     *flags = "h"
+  const char     *flags = "Hh"
     "1:2:3:4:5:6:7:8:"
     "a:r:s:t:c:m:d:f:p:g:bv"
     "A:R:S:T:C:M:D:F:P:G:BV";
@@ -99,7 +98,7 @@ cleanup()
 {
   cleanup_math();
   cleanup_display();
-  close(snd);
+  close_sound_card();
 }
 
 /* die on malloc error */
@@ -110,19 +109,6 @@ nomalloc(char *file, int line)
   perror(error);
   cleanup();
   exit(1);
-}
-
-/* abort and show system error if given ioctl status is bad */
-void
-check_status(int status, int line)
-{
-  if (status < 0) {
-    sprintf(error, "%s: error from sound device ioctl at line %d",
-	    progname, line);
-    perror(error);
-    cleanup();
-    exit(1);
-  }
 }
 
 /* initialize the scope */
@@ -159,36 +145,6 @@ init_channels()
     ch[i].func = i < 2 ? i : FUNCMEM;
     ch[i].mem = 0;
   }
-}
-
-/* [re]initialize /dev/dsp */
-void
-init_sound_card(int firsttime)
-{
-  int parm;
-
-  if (!firsttime)		/* resetting parameters? */
-    close(snd);
-  snd = open("/dev/dsp", O_RDONLY);
-  if (snd < 0) {		/* open DSP device for read */
-    sprintf(error, "%s: cannot open /dev/dsp", progname);
-    perror(error);
-    cleanup();
-    exit(1);
-  }
-
-  parm = 2;			/* set mono/stereo */
-  check_status(ioctl(snd, SOUND_PCM_WRITE_CHANNELS, &parm), __LINE__);
-
-  parm = 8;			/* set 8-bit samples */
-  check_status(ioctl(snd, SOUND_PCM_WRITE_BITS, &parm), __LINE__);
-
-  /* set DMA buffer size */
-  check_status(ioctl(snd, SOUND_PCM_SUBDIVIDE, &scope.dma), __LINE__);
-
-  /* set sampling rate */
-  check_status(ioctl(snd, SOUND_PCM_WRITE_RATE, &scope.rate), __LINE__);
-  check_status(ioctl(snd, SOUND_PCM_READ_RATE, &actual), __LINE__);
 }
 
 /* scale num upward like a scope does, 1 to 200 */
@@ -301,15 +257,9 @@ handle_key(unsigned char c)
   case '0':
     if (scope.run)
       if (actual <= 8800) {
-	scope.rate = 22000;
-	check_status(ioctl(snd, SOUND_PCM_SYNC, 0), __LINE__);
-	check_status(ioctl(snd, SOUND_PCM_WRITE_RATE, &scope.rate), __LINE__);
-	check_status(ioctl(snd, SOUND_PCM_READ_RATE, &actual), __LINE__);
+	set_sound_card(22000);
       } else if (actual <= 22000) {
-	scope.rate = 44000;
-	check_status(ioctl(snd, SOUND_PCM_SYNC, 0), __LINE__);
-	check_status(ioctl(snd, SOUND_PCM_WRITE_RATE, &scope.rate), __LINE__);
-	check_status(ioctl(snd, SOUND_PCM_READ_RATE, &actual), __LINE__);
+	set_sound_card(44000);
       } else
 	scaleup(&scope.scale);
     else if (actual >= 44000)
@@ -321,15 +271,9 @@ handle_key(unsigned char c)
       if (actual <= 8800) {
 	/* average several samples into each pixel */
       } else if (actual <= 22000) {
-	scope.rate = 8800;
-	check_status(ioctl(snd, SOUND_PCM_SYNC, 0), __LINE__);
-	check_status(ioctl(snd, SOUND_PCM_WRITE_RATE, &scope.rate), __LINE__);
-	check_status(ioctl(snd, SOUND_PCM_READ_RATE, &actual), __LINE__);
+	set_sound_card(8800);
       } else if (scope.scale == 1) {
-	scope.rate = 22000;
-	check_status(ioctl(snd, SOUND_PCM_SYNC, 0), __LINE__);
-	check_status(ioctl(snd, SOUND_PCM_WRITE_RATE, &scope.rate), __LINE__);
-	check_status(ioctl(snd, SOUND_PCM_READ_RATE, &actual), __LINE__);
+	set_sound_card(22000);
       } else
 	scaledown(&scope.scale);
     else
@@ -440,51 +384,6 @@ handle_key(unsigned char c)
   default:
     c = 0;			/* ignore unknown keys */
   }
-}
-
-/* get data from sound card, return value is whether we triggered or not */
-int
-get_data()
-{
-  static unsigned char datum[2], prev[2], *buff;
-  int i = 0;
-
-  /* flush the sound card's buffer */
-  check_status(ioctl(snd, SNDCTL_DSP_RESET), __LINE__);
-  read(snd, junk, SAMPLESKIP);	/* toss some possibly invalid samples */
-  if (scope.trige == 1) {
-    read(snd, datum, 2);	/* look for rising edge */
-    do {
-      memcpy(prev, datum, 2);	/* remember previous, read channels */
-      read(snd, datum, 2);
-    } while (((i++ < h_points)) && ((datum[scope.trigch] < scope.trig) ||
-				    (prev[scope.trigch] >= scope.trig)));
-  } else if (scope.trige == 2) {
-    read(snd, datum, 2);	/* look for falling edge */
-    do {
-      memcpy(prev, datum, 2);	/* remember previous, read channels */
-      read(snd, datum, 2);
-    } while (((i++ < h_points)) && ((datum[scope.trigch] > scope.trig) ||
-				    (prev[scope.trigch] <= scope.trig)));
-  }
-  if (i > h_points)		/* haven't triggered within the screen */
-    return(0);			/* give up and keep previous samples */
-
-  clip = 0;
-  memcpy(buffer, prev, 2);	/* now get the post-trigger data */
-  memcpy(buffer + 2, datum, 2);
-  read(snd, buffer + (scope.trige ? 4 : 0),
-       h_points * 2 - (scope.trige ? 4 : 0));
-  buff = buffer;
-  for(i=0; i < h_points; i++) {	/* move it into channel 1 and 2 */
-    if (*buff == 0 || *buff == 255)
-      clip = 1;
-    ch[0].data[i] = (short)(*buff++) - 128;
-    if (*buff == 0 || *buff == 255)
-      clip = 2;
-    ch[1].data[i] = (short)(*buff++) - 128;
-  }
-  return(1);
 }
 
 /* main program */
