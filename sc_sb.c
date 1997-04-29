80a738331639a40f08f59b95ae758777f8d9c6bf
twitham <twitham>
1997-04-29 05:08:18 +0000

    Tossed commented junk.  Updated for 1.3.  Still doesn't work.

--- sc_sb.c
+++ sc_sb.c
@@ -1,7 +1,7 @@
 /*
- * @(#)$Id: sc_sb.c,v 1.1 1997/04/26 01:41:57 twitham Exp $
+ * @(#)$Id: sc_sb.c,v 1.2 1997/04/29 05:08:18 twitham Exp $
  *
- * Copyright (C) 1996 Tim Witham <twitham@pcocd2.intel.com>
+ * Copyright (C) 1997 Tim Witham <twitham@pcocd2.intel.com>
  *
  * (see the files README and COPYING for more details)
  *
@@ -16,22 +16,13 @@
 #include "sb_lib.h"
 #include "sbdetect.h"
 #include "dsp.h"
-/* #include "dma.h" */
+#include "dma.h"
 #include "oscope.h"		/* program defaults */
-
-/* int audio_port=0, audio_irq=0, audio_dma=0, audio_dma16=0; */
-/* int SBok=0; */
-/* struct dsp_device_caps *caps; */
-/* int port, dma, irq,dma16; */
 
 void
 close_sound_card()
 {
   sb_uninstall_driver();
-/*   if (SBok) { */
-/*     dsp_reset(); */
-/*     dsp_close(); */
-/*   } */
 }
 
 void
@@ -43,13 +34,6 @@ set_sound_card(unsigned int rate)
     exit(1);
   }
   actual = sb_sample_frequency;
-/*  dma_reset(SBdma); */		/* this needed? */
-/*  dsp_reset(); */
-
-/*   rate = min(rate, caps->max_8bit_rec_s); */
-/*   rate = max(rate, caps->min_speed); */
-
-/*    actual = dsp_set_record(rate, 1, 8, 0); */
 }
 
 /* [re]initialize /dev/dsp */
@@ -76,25 +60,6 @@ init_sound_card(int firsttime)
   cprintf(" detected at Address %xH, IRQ %d\n\r",sb_info.reset-6,sb_info.IRQ);
   cprintf("Using a sampling rate of %d",sb_sample_frequency);
 
-  
-  /*   int parm; */
-
-  /*   if (!firsttime) */
-  /*     close_sound_card(); */
-
-  /*   sb_get_params(&port,&dma,&irq,&dma16); */
-
-  /*   SBok = dsp_open(port, dma, irq, dma16, h_points * 2, 2); */
-
-  /*   if (!SBok) { */
-  /*     sprintf(error, "%s: Can't open sound device", progname); */
-  /*     perror(error); */
-  /*     cleanup(); */
-  /*     exit(1); */
-  /*   } */
-
-  /*   caps = dsp_get_device_caps(); */
-
   set_sound_card(scope.rate);
 }
 
@@ -104,17 +69,10 @@ get_data()
 {
   static unsigned char *prev, *buff;
   int i = 0;
-/*   static char *p; */
 
   /* flush the sound card's buffer */
 
-/*   sb_dspReset(); */
-/*   p = buffer; */
-/*   while (p < buffer + 640) { */
-/*     *p = (char)sb_dspRead; */
-/*     p++; */
-/*   } */
-  sb_dma8bitReadSC(buffer, 640);
+  sb_dma8bitReadSC((DWORD)buffer, MAXWID * 2);
   prev = buffer + SAMPLESKIP;
   buff = buffer + SAMPLESKIP + 2;
   if (scope.trige == 1) {
@@ -130,18 +88,18 @@ get_data()
       buff += 2;
     }
   }
-  if (i > h_points)
-    return(0);
+  if (i > h_points)		/* haven't triggered within the screen */
+    return(0);			/* give up and keep previous samples */
 
   clip = 0;
-  buff = (scope.trige ? prev : buffer);
-  for(i=0; i < h_points; i++) {
+  buff = scope.trige ? prev : buffer;
+  for(i=0; i < h_points; i++) {	/* move it into channel 1 and 2 */
     if (*buff == 0 || *buff == 255)
       clip = 1;
-    ch[0].data[i] = (short)(*buff++) - 128;
+    mem[23].data[i] = (short)(*buff++) - 128;
     if (*buff == 0 || *buff == 255)
       clip = 2;
-    ch[1].data[i] = (short)(*buff++) - 128;
+    mem[24].data[i] = (short)(*buff++) - 128;
   }
   return(1);
 }
