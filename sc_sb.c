8a130eff950e2ae4d5516bf34170b04c725c8c9f
twitham <twitham>
1997-06-07 21:33:02 +0000

    Finally works!  Mono only.  Thanks to code from sb02.zip.

--- sc_sb.c
+++ sc_sb.c
@@ -1,11 +1,11 @@
 /*
- * @(#)$Id: sc_sb.c,v 1.6 1997/05/31 19:36:39 twitham Exp $
+ * @(#)$Id: sc_sb.c,v 1.7 1997/06/07 21:33:02 twitham Rel $
  *
  * Copyright (C) 1997 Tim Witham <twitham@pcocd2.intel.com>
  *
  * (see the files README and COPYING for more details)
  *
- * This file doesn't yet implement the DOS sound card interface
+ * This file implements the (mono only) DOS/DJGPP Sound Blaster interface
  *
  */
 
@@ -13,11 +13,10 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <conio.h>
-#include "sb_lib.h"
-#include "sbdetect.h"
-#include "dsp.h"
-#include "dma.h"
+#include "sb.h"
 #include "oscope.h"		/* program defaults */
+
+extern int sb_dma_active;	/* in sb.c */
 
 int snd = 0;			/* whether sound is on or off */
 
@@ -26,7 +25,7 @@ void
 close_sound_card()
 {
   if (snd)
-    sb_uninstall_driver();
+    sb_cleanup();
   snd = 0;
 }
 
@@ -35,12 +34,8 @@ int
 set_sound_card(int rate, int chan, int bits)
 {
   if (!snd) return(rate);
-  sb_uninstall_driver();
-  if(sb_install_driver(rate)!=SB_SUCCESS) {
-    fprintf(stderr,"Driver error: %s",sb_driver_error);
-    snd = 0;
-  }
-  return(sb_sample_frequency);
+  
+  return(sb_set_sample_rate(rate));
 }
 
 /* [re]set the sound card, and return actual sample rate */
@@ -56,66 +51,52 @@ reset_sound_card(int rate, int chan, int bits)
 void
 open_sound_card(int dma)
 {
-  if(sb_install_driver(scope.rate)!=SB_SUCCESS) {
-    fprintf(stderr,"Driver error: %s",sb_driver_error);
+  int i;
+
+  if(!(i = sb_init())) {
+    fprintf(stderr,"%s: can't init soundcard!\n", progname);
     snd = 0;
-  } else
+  } else {
+    printf("using soundcard at 0x%x.\n", i);
     snd = 1;
-  set_sound_card(44000, 2, 8);
-  clrscr();
-
-  if(sb_info.dspVersion>=0x040C)
-    cprintf("Sound-Blaster AWE32");
-  else if(sb_info.dspVersion>=0x0400)
-    cprintf("Sound-Blaster 16");
-  else if(sb_info.dspVersion>=0x0300)
-    cprintf("Sound-Blaster Pro");
-  else if(sb_info.dspVersion>=0x0201)
-    cprintf("Sound-Blaster 2.0");
-  else
-    cprintf("Sound-Blaster 1.0/1.5");
-
-  cprintf(" detected at Address %xH, IRQ %d\n\r",sb_info.reset-6,sb_info.IRQ);
-  cprintf("Using a sampling rate of %d",sb_sample_frequency);
+    set_sound_card(44000, 2, 8);
+  }
 }
 
 /* get data from sound card, return value is whether we triggered or not */
 int
 get_data()
 {
-  static unsigned char *prev, *buff, buffer[MAXWID * 2];
+  static unsigned char *prev, *buff, buffer[MAXWID * 2 + SAMPLESKIP];
+  static int triggered;
   int i = 0;
 
   if (!snd) return(0);		/* device open? */
 
-  /* flush the sound card's buffer */
-  sb_dma8bitReadSC((DWORD)buffer, MAXWID * 2 + SAMPLESKIP);
+  if (sb_dma_active) return(triggered);
+
+  sb_rec(buffer, h_points * 2);
   prev = buffer + SAMPLESKIP;
-  buff = buffer + SAMPLESKIP + 2;
+  buff = buffer + SAMPLESKIP + 1;
   if (scope.trige == 1) {
-    while (((i++ < h_points)) && ((*(buff + scope.trigch) < scope.trig) ||
-				  (*(prev + scope.trigch) >= scope.trig))) {
-      prev += 2;
-      buff += 2;
+    while (((i++ < h_points)) && ((*(buff) < scope.trig) ||
+				  (*(prev) >= scope.trig))) {
+      prev += 1; buff += 1;
     }
   } else if (scope.trige == 2) {
-    while (((i++ < h_points)) && ((*(buff + scope.trigch) > scope.trig) ||
-				    (*(prev + scope.trigch) <= scope.trig))) {
-      prev += 2;
-      buff += 2;
+    while (((i++ < h_points)) && ((*(buff) > scope.trig) ||
+				  (*(prev) <= scope.trig))) {
+      prev += 1; buff += 1;
     }
   }
   if (i > h_points)		/* haven't triggered within the screen */
-    return(0);			/* give up and keep previous samples */
+    return(triggered = 0);	/* give up and keep previous samples */
 
-  buff = scope.trige ? prev : buffer;
+  buff = prev;
   for(i=0; i < h_points; i++) {	/* move it into channel 1 and 2 */
     if (*buff == 0 || *buff == 255)
       clip = 1;
-    mem[23].data[i] = (short)(*buff++) - 128;
-    if (*buff == 0 || *buff == 255)
-      clip = 2;
-    mem[24].data[i] = (short)(*buff++) - 128;
+    mem[23].data[i] = mem[24].data[i] = (short)(*buff++) - 128;
   }
-  return(1);
+  return(triggered = 1);
 }
