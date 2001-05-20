68d7d6cf6b3a52ee55643be8896ca058059393fb
twitham <twitham>
2001-05-20 03:00:21 +0000

    Added bitscope dialog that was generated by glade.  So far it's a standalone program, not yet connected to xoscope.

---
+++ bitscope-glade-src/callbacks.h
@@ -0,0 +1,34 @@
+#include <gtk/gtk.h>
+
+extern GtkWidget *window;
+extern guchar dialog_r[];
+
+void
+on_toggled                             (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_value_changed                       (GtkAdjustment *adj,
+                                        gpointer         user_data);
+
+void
+on_notebook1_switch_page               (GtkNotebook     *notebook,
+                                        GtkNotebookPage *page,
+                                        gint             page_num,
+                                        gpointer         user_data);
+
+void
+on_ok                                  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_apply                               (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_entry1_changed                      (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_entry1_changed                      (GtkEditable     *editable,
+                                        gpointer         user_data);
