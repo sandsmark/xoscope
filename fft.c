8f63064dca12570779b0f5009daa2bd536f39b20
Brent Baccala <baccala@freesoft.org>
2014-12-01 20:44:11 -0900

    fix indents and remove trailing spaces in fft.c

--- fft.c
+++ fft.c
@@ -4,7 +4,7 @@
  *
  * (see the files README and COPYING for more details)
  *
- * This file implements the interface to the FFTW-library for xoscope.  
+ * This file implements the interface to the FFTW-library for xoscope.
  *
  */
 #include <stdio.h>
@@ -13,7 +13,7 @@
 #include <math.h>
 #include <fftw3.h>
 #include "xoscope.h"
-#include "fft.h"       
+#include "fft.h"
 
 double          *dp = NULL;
 fftw_complex    *cp = NULL;
@@ -25,56 +25,55 @@ void fftW(short *in, short *out, int inLen)
 {
     double  r, i, mag;
     int     k, max;
-    
-    for(k = 0; k < inLen && k < FFTLEN; k++) {
-            dp[k] = (double)in[k];
+
+    for (k = 0; k < inLen && k < FFTLEN; k++) {
+        dp[k] = (double)in[k];
     }
 
-        fftw_execute(plan);
+    fftw_execute(plan);
 
-        max = (FFTLEN / 2) + 1;
-        for (k = 0; k < max; k++) {
-                r = cp[k][0];
-                i = cp[k][1];
-                mag = sqrt((r * r) + (i * i));
-/*              printf("%d %lf %lf %lf\n", k, r, i, mag);*/
+    max = (FFTLEN / 2) + 1;
+    for (k = 0; k < max; k++) {
+        r = cp[k][0];
+        i = cp[k][1];
+        mag = sqrt((r * r) + (i * i));
+        /*              printf("%d %lf %lf %lf\n", k, r, i, mag);*/
 
         if (mag >= (1<<(sizeof(short) * 8 - 1))) {              /* avoid overflowing the short */
             mag = (1<<(sizeof(short) * 8 - 1)) - 1;     /* max short = 2^15 */
-        } 
-                out[k] = (short)mag;
         }
+        out[k] = (short)mag;
+    }
 }
 
 void InitializeFFTW(int fftlen)
 {
-        if(dp == NULL){
-            if((dp = (double *)malloc(sizeof (double) * fftlen)) == NULL){
+    if (dp == NULL) {
+        if ((dp = (double *)malloc(sizeof (double) * fftlen)) == NULL) {
             fprintf(stderr, "malloc failed in InitializeFFTW()\n");
-                exit(0);
-            }
-        } 
-        memset(dp, 0, sizeof (double) * fftlen);
-            
-        if(cp == NULL){
-        if((cp = (fftw_complex *)fftw_malloc(sizeof (fftw_complex) * fftlen)) == NULL){;
-            fprintf(stderr, "malloc failed in InitializeFFTW()\n");
-                exit(0);
-            }
+            exit(0);
         }
-        memset(cp, 0, sizeof (fftw_complex) * fftlen);
+    }
+    memset(dp, 0, sizeof (double) * fftlen);
 
-        plan = fftw_plan_dft_r2c_1d(fftlen, dp, cp, FFTW_MEASURE);
+    if (cp == NULL) {
+        if ((cp = (fftw_complex *)fftw_malloc(sizeof (fftw_complex) * fftlen)) == NULL) {
+            fprintf(stderr, "malloc failed in InitializeFFTW()\n");
+            exit(0);
+        }
+    }
+    memset(cp, 0, sizeof (fftw_complex) * fftlen);
+
+    plan = fftw_plan_dft_r2c_1d(fftlen, dp, cp, FFTW_MEASURE);
 }
 
 void EndFFTW(void)
 {
-        fftw_destroy_plan(plan);
+    fftw_destroy_plan(plan);
 
     free(dp);
     dp = NULL;
-    
+
     free(cp);
     cp = NULL;
 }
-
